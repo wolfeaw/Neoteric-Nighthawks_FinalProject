329dd772db7926e31d18cdb2e19116548ba2860c
wolfeaw <wolfeaw@mail.uc.edu>
2024-12-05 13:22:20 -0500

    needed a  capital letter changed

--- Neoteric Nighthawks_FinalProject/index.aspx.cs
+++ Neoteric Nighthawks_FinalProject/index.aspx.cs
@@ -7,9 +7,9 @@ using System.Web.UI.WebControls;
 
 namespace Neoteric_Nighthawks_FinalProject
 {
-    public partial class Index : System.Web.UI.Page
+    public partial class index : System.Web.UI.Page
     {
-        protected void page_Load(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)
         {
 
         }
